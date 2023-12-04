193c73ec4f5f80dddd419926dc93c1140c7cd10d
34435345io. 3 <152788646+eduffield9@users.noreply.github.com>
2023-12-04 16:49:21 -0600

    Create indian.asm

---
+++ indian.asm
@@ -0,0 +1,22 @@
+copengagagn o234 ioio23io 4  
+JH2 3
+4 
+23 
+4234  addresso23 o4 io23o i4 oi234 
+
+2
+ 34
+ 2 
+34
+23
+4  adresso2 io34o in2no3i4o n2 oni34 ooi 23 4 
+23 4
+
+ 23
+ 4 
+23
+42  birth 2io34 io2io 34 ion i2on3 oinoi234 ohio io234n iino 23io 4 ino2 
+@ 34 
+
+234  
+
