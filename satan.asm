72f74c14b24c3f9d24cce2cc3fc785f409ed3add
34435345io. 3 <152788646+eduffield9@users.noreply.github.com>
2023-12-04 16:50:15 -0600

    Create satan.asm

---
+++ satan.asm
@@ -0,0 +1,35 @@
+bsar satan o23 4io ino2o34
+jh 2
+34 
+234  fiend io2 ino34 oi23 io4 oi23o i4 oii 234 
+ 2 
+34
+ 234 fiend 
+2 3
+4
+ 234 shiuo2 er io23u4n  io234o i nio2 
+
+
+h 234
+ j234  34 243 
+
+
+ha
+ha  bar oi23o i4 noi23oin 4on 2no ioi noi234 
+h 2
+23234  girkl 2oi34 io ion2oin 234 
+4 4
+2323
+ h4234 yellow io nio23 ion4 ion2ion34noi n io 234 
+h2323
+ 4 4
+2323
+4234234  inty  2io34 nio2n 3io4 nio2o ino234  
+ 223
+344
+ 2323
+ 4324 yellow ino 2iop34 noioi 234 
+ 2323
+ 44
+ 2323
+ 4234uo2 io3 ino2nio34oin oi 234 nio ooi n23 oiu io23oi4 234 
