7bc10cb3cb426b72b19e75ef385affb3ac39238b
channel i io2 on 2no3 nio23 io nio23n io oi23io n23i o2 234 <152788646+eduffield9@users.noreply.github.com>
2023-12-08 14:11:44 -0600

    Update indian.asm

--- indian.asm
+++ indian.asm
@@ -1,3 +1,25 @@
+i wont drive  there oi 23o4in23iooin23 io234234oioi23l23n4 ;23oin2i3oin234 
+2 3
+ 2
+
+ 23
+ h4h
+2
+h32 34  they oi23 oi2 ioi234oi oi2 34
+234 h
+ 23  i23  234  
+234 the 9i2o34 oi 234 
+
+234 
+23 they 2io3 io234 
+4h
+23  ad oin2 3oi nio2 234 asm oi2n3 io io2 234  door they oi2 oi2 ni234 
+hh23
+
+2
+234 
+   net oi23 4io io2noi23in 23o4 next 2384 23o4in 2iooin2 34 oi2 23 4
+
 copengagagn o234 ioio23io 4  
 JH2 3
 4 
